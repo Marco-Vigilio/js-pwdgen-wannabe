b015d32cd8496f212f2e15e2054c72dc94ab36d2
Marco-Vigilio <tonyvigilio@gmail.com>
2023-05-11 15:29:16 +0200

    inserimento di bootstrap

--- script/myscript.js
+++ script/myscript.js
@@ -6,14 +6,14 @@ let cognome;
 let colore;
 
 nome = prompt ('Inserisci il tuo nome: ');
-document.getElementById('name').innerHTML = "Il tuo nome è: " + nome;
+document.getElementById('my_name').innerHTML = "Il tuo nome è: " + nome;
 
 cognome = prompt ('Inserisci il tuo Cognome: ');
-document.getElementById('surname').innerHTML = "Il tuo cognome è: " + cognome;
+document.getElementById('my_surname').innerHTML = "Il tuo cognome è: " + cognome;
 
 colore = prompt ('Inserisci il tuo colore preferito: ');
-document.getElementById('color').innerHTML = "Il tuo colore preferito è: " + colore;
+document.getElementById('my_color').innerHTML = "Il tuo colore preferito è: " + colore;
 
-document.getElementById('result').innerHTML = nome+cognome+colore+"21";
+document.getElementById('my_result').innerHTML = nome+cognome+colore+"21";
 
-document.getElementById('Result').innerHTML = "nomecognomecolorepreferito21";
+document.getElementById('my_Result').innerHTML = "nomecognomecolorepreferito21";
