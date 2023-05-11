5d032fed50318839fbe6788e5f15768df54faab2
Marco-Vigilio <tonyvigilio@gmail.com>
2023-05-11 14:59:40 +0200

    inserisco descrizione prima del dato

--- script/myscript.js
+++ script/myscript.js
@@ -6,13 +6,13 @@ let cognome;
 let colore;
 
 nome = prompt ('Inserisci il tuo nome: ');
-document.getElementById('name').innerHTML = nome;
+document.getElementById('name').innerHTML = "Il tuo nome è: " + nome;
 
 cognome = prompt ('Inserisci il tuo Cognome: ');
-document.getElementById('surname').innerHTML = cognome;
+document.getElementById('surname').innerHTML = "Il tuo cognome è: " + cognome;
 
 colore = prompt ('Inserisci il tuo colore preferito: ');
-document.getElementById('color').innerHTML = colore;
+document.getElementById('color').innerHTML = "Il tuo colore preferito è: " + colore;
 
 document.getElementById('result').innerHTML = nome+cognome+colore+"21";
 
